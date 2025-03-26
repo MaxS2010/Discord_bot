7c8250302c0f853f6fd47f1bec9271464f655c17
MaxSubach <subachmax2010@gmail.com>
2025-03-26 16:27:18 +0200

    Second commit

--- modules/discord_bot.py
+++ modules/discord_bot.py
@@ -1,5 +1,9 @@
-import discord
+import discord, dotenv, os
 
+# Отримання змінної середовища
+dotenv.load_dotenv()
+# Отримання токену змінної середовища
+TOKEN = os.getenv("TOKEN")
 
 # Встановлює дозволів, зокрема до відстежування повідомлень
 intents = discord.Intents.default()
@@ -8,20 +12,19 @@ intents.message_content = True
 # Створюємо клієнтаь та передаємо йому дозволи
 bot = discord.Client(intents= intents)
 
-# 
+# Створення події, яка відбувається при запуску бота
 @bot.event
 async def on_ready():
     print("Bot is ready")
 
-# 
 @bot.event
-# 
+# Створення події, яка відбувається при отриманні повідомлення
 async def on_message(message):
-    # 
+    # Отримання контенту повідомлення
     content = message.content
-    # 
+    # Якщо автор повідомлення не бот
     if message.author != bot.user:
-        # 
+        # Якщо повідомлення не пусте
         if content:
-            # 
+            # Відправляємо повідомлення с таким самим контентом
             await message.channel.send(content)
